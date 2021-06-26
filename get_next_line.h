b1ec650c116f33385b2693a9dfe52c1af19a0028
Jorge Alves <jorgeedualves84@gmail.com>
2021-06-26 16:55:56 -0300

    Projeto Finalizado sem Bonus

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/16 23:46:18 by joeduard          #+#    #+#             */
+/*   Updated: 2021/06/16 23:46:18 by joeduard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+int				get_next_line(int fd, char **line);
+char			*ft_strdup(const char *s1);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strchr(const char *s, int c);
+size_t			ft_strlen(const char *s);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
